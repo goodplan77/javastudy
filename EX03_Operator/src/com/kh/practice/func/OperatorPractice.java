ee37ba34b09a7c83db983d0121bab5b8acc85d94
user1 <user1@DESKTOP-E24V8TR>
2024-03-20 17:49:37 +0900

    homework in progress

---
+++ EX03_Operator/src/com/kh/practice/func/OperatorPractice.java
@@ -0,0 +1,51 @@
+package com.kh.practice.func;
+
+import java.util.Scanner;
+
+public class OperatorPractice {
+
+	Scanner sc = new Scanner(System.in);
+
+	public void practice1() {
+			System.out.print("인원 수 : ");
+			int count = sc.nextInt();
+			
+			System.out.print("사탕 개수 : ");
+			int candy = sc.nextInt();
+			
+			System.out.println("1인당 사탕 개수 : " + (candy / count));
+			System.out.println("남는 사탕 개수 : " + (candy % count));
+	}
+
+	public void practice2() {
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+		
+
+	}
+
+	public void practice3() {
+
+	}
+
+	public void practice4() {
+
+	}
+
+	public void practice5() {
+
+	}
+
+	public void practice6() {
+
+	}
+
+	public void practice7() {
+
+	}
+
+	public void practice8() {
+
+	}
+
+}
---
+++ EX03_Operator/src/com/kh/practice/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.practice.run;
+
+import com.kh.practice.func.OperatorPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		OperatorPractice op = new OperatorPractice();
+		op.practice1();
+
+	}
+
+}
