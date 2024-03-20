611b6871b666f5b7b831f4d0d7dbac4fb9d594f8
goodp <goodp@DESKTOP-RQDOQ74>
2024-03-20 22:59:35 +0900

    homework in progess2

--- EX03_Operator/src/com/kh/practice/func/OperatorPractice.java
+++ EX03_Operator/src/com/kh/practice/func/OperatorPractice.java
@@ -21,6 +21,21 @@ public class OperatorPractice {
 		System.out.print("이름 : ");
 		String name = sc.nextLine();
 		
+		System.out.print("학년(숫자만) : ");
+		int grade = sc.nextInt();
+		
+		System.out.print("반(숫자만) : ");
+		int classes = sc.nextInt();
+		
+		System.out.print("번호(숫자만) : ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.print("성별(M/F) : ");
+		String gender = sc.nextLine();
+		
+		System.out.print("성적(소수점 아래 둘째자리까지) : ");
+		
 
 	}
 
