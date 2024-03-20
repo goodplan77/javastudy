5682a0b22e3185f30ea892b48e91181a644ca4b2
user1 <user1@DESKTOP-E24V8TR>
2024-03-20 14:11:43 +0900

    commit2

---
+++ EX_Variable2/src/com/kh/practice2/func/CastingPractice.java
@@ -0,0 +1,28 @@
+package com.kh.practice2.func;
+
+import java.util.Scanner;
+
+public class CastingPractice {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void subject() {
+		
+		System.out.print("국어 : ");
+		double KoreanScore = sc.nextDouble();
+		
+		System.out.print("영어 : ");
+		double EnglishScore = sc.nextDouble();
+		
+		System.out.print("수학 : ");
+		double MathScore = sc.nextDouble();
+		
+		int total = (int)KoreanScore + (int)EnglishScore + (int)MathScore;
+		int average = total / 3 ;
+		
+		System.out.println("총점 : " + total);
+		System.out.println("평균 : " + average);
+		
+	}
+
+}
---
+++ EX_Variable2/src/com/kh/practice2/run/Run.java
@@ -0,0 +1,17 @@
+package com.kh.practice2.run;
+
+import com.kh.practice2.func.*;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		//CastingPractice cp = new CastingPractice();
+		// cp.subject();
+		
+		CastingPractice2 cp2 = new CastingPractice2();
+		cp2.method();
+
+	}
+
+}
