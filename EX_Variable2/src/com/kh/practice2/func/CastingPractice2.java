8b36477462cb3912c3a91fe15c7ddf240ade560d
user1 <user1@DESKTOP-E24V8TR>
2024-03-20 15:11:35 +0900

    change1

--- EX_Variable2/src/com/kh/practice2/func/CastingPractice2.java
+++ EX_Variable2/src/com/kh/practice2/func/CastingPractice2.java
@@ -10,7 +10,7 @@ public class CastingPractice2 {
 		char ch = 'A';
 		System.out.println(iNum1 / iNum2 ); // 2
 		System.out.println((int)dNum ); // 2
-		System.out.println( (double)iNum2 + (3*(int)dNum)); // 10.0
+		System.out.println( iNum2 * dNum); // 10.0
 		System.out.println((double)iNum1 ); // 10.0
 		System.out.println((double)iNum1 / (double)iNum2 ); // 2.5
 		System.out.println( dNum); // 2.5
