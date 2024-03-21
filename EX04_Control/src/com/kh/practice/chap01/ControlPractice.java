fcc8e5e5de7f117075d4ce14d12f0b135eb2b08e
goodplan77 <goodplan77@naver.com>
2024-03-21 16:29:08 +0900

    Control Practice file

--- EX04_Control/src/com/kh/practice/chap01/ControlPractice.java
+++ EX04_Control/src/com/kh/practice/chap01/ControlPractice.java
@@ -106,6 +106,192 @@ public class ControlPractice {
 		String inputID = sc.nextLine();
 		System.out.print("비밀번호 : ");
 		String inputPassword = sc.nextLine();
+
+		if (!inputID.equals(ID)) {
+			System.out.println("아이디가 틀렸습니다.");
+
+		} else if (!inputPassword.equals(password)) {
+			System.out.println("비밀번호가 틀렸습니다.");
+		} else {
+			System.out.println("로그인 성공");
+		}
+	}
+
+	public void practice6() {
+		System.out.print("권한을 확인하고자 하는 회원 등급 : ");
+		String outh = sc.nextLine();
+
+		switch (outh) {
+		case "관리자":
+			System.out.println("회원관리, 게시글 관리 게시글 작성, 댓글 작성, 게시글 조회");
+			break;
+		case "회원":
+			System.out.println("게시글 작성, 게시글 조회, 댓글 작성");
+			break;
+		case "비회원":
+			System.out.println("게시글 조회");
+			break;
+		default:
+			System.out.println("잘못된 접근");
+			break;
+		}
+	}
+
+	public void practice7() {
+		System.out.print("키(m)를 입력해 주세요. :");
+		double height = sc.nextDouble();
+
+		System.out.print("키(m)를 입력해 주세요. :");
+		double weight = sc.nextDouble();
+
+		double bmi = weight / (height * height);
+
+		if (bmi < 18.5) {
+			System.out.println("저체중");
+		} else if (bmi < 23) {
+			System.out.println("정상체중");
+		} else if (bmi < 25) {
+			System.out.println("과체중");
+		} else if (bmi < 30) {
+			System.out.println("비만");
+		} else {
+			System.out.println("고도 비만");
+		}
+
+	}
+
+	public void practice8() {
+		System.out.print("피연산자1 입력 : ");
+		int num1 = sc.nextInt();
+
+		System.out.print("피연산자2 입력 : ");
+		int num2 = sc.nextInt();
+		sc.nextLine();
+
+		if (num1 > 0 && num2 > 0) {
+			System.out.print("연산자를 입력(+,-,*,/,%) : ");
+			char operator = sc.nextLine().charAt(0);
+
+			double output;
+
+			switch (operator) {
+			case '+':
+				output = num1 + num2;
+				System.out.printf("%d %c %d = %.0f", num1, operator, num2, output);
+				break;
+			case '-':
+				output = num1 - num2;
+				System.out.printf("%d %c %d = %.0f", num1, operator, num2, output);
+				break;
+			case '*':
+				output = num1 * num2;
+				System.out.printf("%d %c %d = %.0f", num1, operator, num2, output);
+				break;
+			case '/':
+				output = (double) num1 / num2;
+				System.out.printf("%d %c %d = %.6f", num1, operator, num2, output);
+				break;
+			case '%':
+				output = num1 % num2;
+				System.out.printf("%d %c %d = %.0f", num1, operator, num2, output);
+				break;
+			default:
+				System.out.println("잘못 입력하셨습니다. 프로그램을 종료합니다.");
+				break;
+			}
+		} else {
+			System.out.println("입력값 모두 양수 여야 합니다.");
+		}
+	}
+
+	public void practice9() {
+		System.out.print("중간 고사 점수 : ");
+		int middleScore = sc.nextInt();
+
+		System.out.print("기말 고사 점수 : ");
+		int finalScore = sc.nextInt();
+
+		System.out.print("과제 점수 : ");
+		int homeworkScore = sc.nextInt();
+
+		System.out.print("출석 회수 : ");
+		int attendCount = sc.nextInt();
+
+		System.out.println("================= 결과 =================");
+
+		if (attendCount < 14) {
+			System.out.printf("FAIL [출석횟수 부족(%d/20)]\n", attendCount);
+
+		} else {
+			System.out.printf("중간 고사 점수(20) : %.1f\n", middleScore * 0.2);
+			System.out.printf("중간 고사 점수(30) : %.1f\n", finalScore * 0.3);
+			System.out.printf("과제 점수(30) : %.1f\n", homeworkScore * 0.3);
+			System.out.printf("출석 점수(20) : %.1f\n", (double) attendCount);
+
+			double score = (middleScore * 0.2) + (finalScore * 0.3) + (homeworkScore * 0.3) + attendCount;
+			System.out.printf("총점 : %.1f\n", score);
+
+			if (score >= 70) {
+				System.out.println("PASS");
+			} else {
+				System.out.println("FAIL");
+			}
+		}
+
+	}
+
+	public void practice10() {
+		System.out.println("실행할 기능을 선택하세요.");
+		System.out.println("1. 메뉴 출력");
+		System.out.println("2. 짝수/홀수");
+		System.out.println("3. 합격/불합격");
+		System.out.println("4. 계절");
+		System.out.println("5. 로그인");
+		System.out.println("6. 권한 확인");
+		System.out.println("7. BMI");
+		System.out.println("8. 계산기");
+		System.out.println("9. P/F");
+
+		int select = sc.nextInt();
+
+		switch (select) {
+		case 1:
+			practice1();
+			break;
+		case 2:
+			practice2();
+			break;
+		case 3:
+			practice3();
+			break;
+		case 4:
+			practice4();
+			break;
+		case 5:
+			practice5();
+			break;
+		case 6:
+			practice6();
+			break;
+		case 7:
+			practice7();
+			break;
+		case 8:
+			practice8();
+			break;
+		case 9:
+			practice9();
+			break;
+		default:
+			System.out.println("유효한 값을 입력하세요. (정수 1 ~ 9)");
+			break;
+
+		}
+
+	}
+
+	public void practice11() {
+
 	}
 
 }
