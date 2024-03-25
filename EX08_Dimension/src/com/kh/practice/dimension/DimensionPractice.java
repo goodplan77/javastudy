7d29ae7a3e2e40cf1f4e5db94145b37cca194310
goodplan77 <goodplan77@naver.com>
2024-03-25 17:52:19 +0900

    Dimension practice file

---
+++ EX08_Dimension/src/com/kh/practice/dimension/DimensionPractice.java
@@ -0,0 +1,59 @@
+package com.kh.practice.dimension;
+
+public class DimensionPractice {
+
+	public void practice1() {
+		String[][] array = new String[3][3];
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				array[i][j] = "(" + i + ", " + j + ")";
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(array[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+	public void practice2() {
+		int[][] array = new int[4][4];
+		int count = 1;
+
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0 ; j < 4 ; j++) {
+				array[i][j] = count++;
+			}
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0 ; j < 4 ; j++) {
+				System.out.printf("%d " , array[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public void practice3() {
+		int[][] array = new int[4][4];
+		int count = 16;
+
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0 ; j < 4 ; j++) {
+				array[i][j] = count--;
+			}
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0 ; j < 4 ; j++) {
+				System.out.printf("%d " , array[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ EX08_Dimension/src/com/kh/practice/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.practice.run;
+
+import com.kh.practice.dimension.DimensionPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		DimensionPractice dp = new DimensionPractice();
+		dp.practice3();
+
+	}
+
+}
