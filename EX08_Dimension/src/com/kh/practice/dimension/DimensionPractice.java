f95ac6848e579d84bd199ae3ef49d543f6c0bf94
goodplan77 <goodplan77@naver.com>
2024-03-25 22:41:16 +0900

    Dimension practice file

--- EX08_Dimension/src/com/kh/practice/dimension/DimensionPractice.java
+++ EX08_Dimension/src/com/kh/practice/dimension/DimensionPractice.java
@@ -25,35 +25,47 @@ public class DimensionPractice {
 		int count = 1;
 
 		for (int i = 0; i < 4; i++) {
-			for(int j = 0 ; j < 4 ; j++) {
+			for (int j = 0; j < 4; j++) {
 				array[i][j] = count++;
 			}
 		}
-		
+
 		for (int i = 0; i < 4; i++) {
-			for(int j = 0 ; j < 4 ; j++) {
-				System.out.printf("%d " , array[i][j]);
+			for (int j = 0; j < 4; j++) {
+				System.out.printf("%d ", array[i][j]);
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public void practice3() {
 		int[][] array = new int[4][4];
 		int count = 16;
 
 		for (int i = 0; i < 4; i++) {
-			for(int j = 0 ; j < 4 ; j++) {
+			for (int j = 0; j < 4; j++) {
 				array[i][j] = count--;
 			}
 		}
-		
+
 		for (int i = 0; i < 4; i++) {
-			for(int j = 0 ; j < 4 ; j++) {
-				System.out.printf("%d " , array[i][j]);
+			for (int j = 0; j < 4; j++) {
+				System.out.printf("%d ", array[i][j]);
 			}
 			System.out.println();
 		}
+	}
+
+	public void practice4() {
+
+		int[][] array = new int[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+						
+			}
+		}
+
 	}
 
 }
