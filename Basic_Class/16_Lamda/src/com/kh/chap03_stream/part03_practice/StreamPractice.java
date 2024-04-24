46d227f26eaa1231f34c6f46461cc1676bb5eb19
goodplan77 <goodplan77@naver.com>
2024-04-24 22:12:24 +0900

    Stream Practice file

--- Basic_Class/16_Lamda/src/com/kh/chap03_stream/part03_practice/StreamPractice.java
+++ Basic_Class/16_Lamda/src/com/kh/chap03_stream/part03_practice/StreamPractice.java
@@ -116,22 +116,53 @@ public class StreamPractice {
 		// 6. slist에서 학생의 이름과 나이를 학생이름기 오름차순 정렬하여 출력.
 		// 이름: 나이
 		// 강감찬 : 16 김말똥 : 29 아무개 : 23 이순신 : 25 홍길동 : 15
-
+		
+		slist.stream()
+			.sorted((o1, o2) -> o1.getName().compareTo(o2.getName()))
+			.forEach(n -> System.out.printf("%s : %d " , n.getName() , n.getAge()));
+		
+		System.out.println();
+		
 		// 7. slist에서 20살 이상인 학생의 평균점수를 구하는 프로그램
 		// 80.0
-		//	      System.out.println(score);
+		// System.out.println(score);
+		int score = slist.stream()
+			.filter(stu -> stu.getAge() >=20)
+			.map(stu -> stu.getScore())
+			.reduce(0 , (init , n ) -> {
+				return init + n;
+			});
+		System.out.println(score);
 
 		// 8. wordArr내부요소의 공백을 모두 제거한후 List<String>으로 변환하는 프로그램
 		// 출력결과 : [abcd, 홍길동, helloworld]
+		
+		List<String> temp2 = Arrays.stream(wordArr)
+									.map(s -> s.replaceAll(" ", ""))
+									.collect(Collectors.toList());
+		System.out.println(temp2);
 
 		// 9. dan을 활용하여 구구단 2단~9단까지 출력하는 프로그램을 만드시오
 		// ex) 2 * 1 = 2
 		// 2 * 2 = 4
-		//	      
+		
+		dan.filter(n -> n>=2)
+			.forEach(n -> {
+				IntStream.range(1, 10)
+						.forEach(a -> {
+							System.out.printf("%d * %d = %d\n",n,a, (n*a));
+						});
+				System.out.println();
+			});
+			
 
 		// 10. wordArr내부요소의 공백을 제거한 문자열의 길이가 8이상인 요소가 있는지 검사하는 프로그램
 		// 출력결과 : true/false값
-
+		
+		boolean bool1 = Arrays.stream(wordArr)
+							.map(s -> s.replaceAll(" ", ""))
+							.anyMatch(s1 -> s1.length() >=8);
+		System.out.println(bool1);
 	}
 
 }
